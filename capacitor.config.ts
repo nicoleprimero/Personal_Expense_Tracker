3bc95289d507ea47746fb31de6d4911c7ddcbdfc
Nicole Primero <nicoleprimero20@gmail.com>
2026-09-12 19:47:27 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Personal_Expense_Tracker',
+  webDir: 'dist'
+};
+
+export default config;
